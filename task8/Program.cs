30ec3da8421096b495d705256ce6104c1848b42d
SergeyStartsev <startcevs@yandex.ru>
2022-08-11 20:43:03 +0500

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите чило: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if(count < N) 
+{
+    while (count <= N)
+    {
+        Console.WriteLine (count);
+        count+=2;
+    }
+}
+else
+{
+        Console.WriteLine ("Введите целое число больше 2");
+}
