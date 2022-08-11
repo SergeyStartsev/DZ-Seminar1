fedba13db16d337501f0b91b799fea05815dda92
SergeyStartsev <startcevs@yandex.ru>
2022-08-11 20:10:47 +0500

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое чило:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB)
+{
+    Console.Write ("max = ");
+    Console.WriteLine (numberA);
+}
+else
+{
+    Console.Write ("max = ");
+    Console.WriteLine (numberB);
+}
