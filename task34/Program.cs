310f45f9d6b926699fc3e097f9f5bb4b8ed6aac9
SergeyStartsev <startcevs@yandex.ru>
2022-08-25 16:43:59 +0500

    task34

---
+++ task34/Program.cs
@@ -0,0 +1,19 @@
+﻿// Массив заполненный случайными положительными трёхзначными числами.Программа, которая покажет количество чётных чисел в массиве.
+
+Console.Clear();
+Console.WriteLine("Введите количество чисел");
+int q = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[q];
+int count = 0;
+for (int i = 0; i < q; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine($"Количество четных чисел в массиве = {count}");
+Console.WriteLine();
+
