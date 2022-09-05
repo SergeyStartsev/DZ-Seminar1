3ee7b5857b42d642e277b3c978a0388cc30c38b5
SergeyStartsev <startcevs@yandex.ru>
2022-09-05 21:20:12 +0500

    task54

---
+++ task54/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Clear();
+const int ROWS = 4;
+const int COLUMNS = 4;
+int[,] FillMatrixRandom(int rows, int columns, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = FillMatrixRandom(ROWS, COLUMNS);
+Console.WriteLine("Исходный массив:");
+PrintMatrix(matrix);
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int k = j + 1; k < matrix.GetLength(1); k++)
+        {
+            if (matrix[i,j] < matrix[i,k])
+            {
+                int temp = matrix[i,k];
+                matrix[i,k] = matrix[i,j];
+                matrix[i,j] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Измененный массив:");
+PrintMatrix(matrix);
