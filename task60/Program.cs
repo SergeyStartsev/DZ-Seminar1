2cbf2b59d657efb8388095e0264550fb2ed17f57
SergeyStartsev <startcevs@yandex.ru>
2022-09-05 21:26:30 +0500

    task60

---
+++ task60/Program.cs
@@ -0,0 +1,45 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Console.Clear();
+const int X = 2;
+const int Y = 2;
+const int Z = 2;
+
+int[,,] FillMatrixRandom(int x, int y, int z, int leftRange = 10, int rightRange = 100)
+{
+    int[,,] matrix = new int[x, y, z];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = rand.Next(leftRange, rightRange);
+            }
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int k = 0; k < matrix.GetLength(2); k++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.WriteLine($"{matrix[i, j, k]} ({i},{j},{k})");
+            }
+        }
+    }
+}
+
+int[,,] matrix = FillMatrixRandom(X, Y, Z);
+PrintMatrix(matrix);
